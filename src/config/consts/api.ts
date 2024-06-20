dfbda750e8b8aefac9af6e50914b21d426e7d9a4
SeyunJang <kj10522002@gmail.com>
2024-06-20 10:42:01 +0900

    update

--- src/config/consts/api.ts
+++ src/config/consts/api.ts
@@ -7,6 +7,7 @@ export const GET_ROUTES = '/map/driving-route';
 export const SEARCH_ON_PATH = '/map/search-on-path';
 export const GET_STOPBY_DURATION = '/map/stopby-duration';
 
-export const LOCAL_LLM_URL = '10.16.135.41'; //FIXME: change this to the local llm server url
+//:11434 <- original port
+export const LOCAL_LLM_URL = 'https://81ae-163-152-3-138.ngrok-free.app'; //FIXME: change this to the local llm server url
 export const LLM_MODEL_NAME = 'gemma';
 export const GET_REVIEW_SUMMARY = '/map/get-review-summary';
